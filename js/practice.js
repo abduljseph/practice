9d718f45eaf7d894b262043c3987c13849f6439d
Abdul Joseph <abduljseph@gmail.com>
2023-11-16 16:41:15 -0600

    more work in practice js

--- js/practice.js
+++ js/practice.js
@@ -1,1 +1,61 @@
 console.log("inside practice.js")
+// //
+// // function isOdd(number)  {
+// //     return number % 2 !== 0;
+// // }
+// //
+// // console.log(isOdd(6));
+// // console.log(isOdd(5));
+// //
+// // function isEven(number) {
+// //     return number % 2===0
+// // }
+// //
+// // console.log(isEven(6));
+// // console.log(isEven(5));
+//
+// function identity(input) {
+//    return input + 2;
+// }
+// console.log(identity(6));
+//
+// function isFive(input) {
+
+// }
+//
+// function addFive(input) {
+//     return input + 5;
+// }
+// console.log(addFive(6));
+//
+// function isMultipleOfFive(input){
+//
+// }
+//
+// function isThree(input) {
+//
+// }
+//
+// function isMultipleOfThree(input) {
+//
+// }
+
+// function isMultipleOfThreeAndFive(input) {
+//
+// }
+
+// function isMultipleOf(target, n) {
+//     if (target % n ===0) {
+//         return "this is evenly divisible";
+//     }
+// }
+// console.log(isMultipleOf(10, 2));
+
+function isTrue(boolean) {
+
+}
+
+function isFalse(boolean) {
+
+}
+
